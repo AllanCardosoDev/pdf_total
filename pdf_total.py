2dce5da16b70805d15e8f1d0e3a90c342df39f01
Allan Cardoso Araujo <112924458+AllanCardosoDev@users.noreply.github.com>
2024-12-16 21:08:01 -0300

    Update pdf_total.py

--- pdf_total.py
+++ pdf_total.py
@@ -3,7 +3,7 @@ import re
 from PyPDF2 import PdfReader, PdfWriter, PdfMerger
 # from reportlab.pdfgen import canvas
 # from reportlab.lib.pagesizes import letter, A4
-from reportlab.lib.colors import Color
+# from reportlab.lib.colors import Color
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from io import BytesIO
